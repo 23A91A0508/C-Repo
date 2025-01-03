18bbfc963d7a36def4f7733d1313f8b92fd55d49
Ballamudi Raghavendra <23A91A0508@aec.edu.in>
2025-01-03 11:40:36 +0530

    Uploading code: Find Second Number.c

---
+++ Find Second Number.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int X, Y, Z;
+
+    // Read the input values
+    scanf("%d %d", &X, &Y);
+
+    // Calculate the second number
+    Z = 2 * X - Y;
+
+    // Output the result
+    printf("%d\n", Z);
+
+    return 0;
+}
